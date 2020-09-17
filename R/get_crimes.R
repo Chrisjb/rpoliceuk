e9f391d27ac963fd6dc0245563c994c38776a172
Chris Bailey <chrisbailey@Chriss-MacBook-Pro.local>
2020-09-17 16:14:39 +0100

    upated project setup

---
+++ R/get_crimes.R
@@ -0,0 +1,22 @@
+#' get_crimes
+#'
+#' Fetches crimes from the police.uk API within a given sf polygon.
+#'
+#' @param polygon an sf polygon within which to request the crimes data
+#' @param months number of months to get data for, defaults to number of months up to the latest date available.
+#'
+#' @return street level crimes as sf points
+#'
+#' @import sf
+#' @import httr
+#' @import dplyr
+#'
+#'
+#' @examples
+#'
+#'
+#' @export
+
+get_crimes <- function(polygon, months){
+
+}
